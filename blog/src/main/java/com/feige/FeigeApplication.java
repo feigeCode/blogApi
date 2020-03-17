504f22e52e0234b62201958bd65a79d4f9632d8e
feigeCode <1835698775@qq.com>
2020-03-17 16:59:20 +0800

    初始化项目

---
+++ blog/src/main/java/com/feige/FeigeApplication.java
@@ -0,0 +1,13 @@
+package com.feige;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeigeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeigeApplication.class, args);
+    }
+
+}
